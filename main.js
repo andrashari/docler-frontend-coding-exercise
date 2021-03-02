32ebad073c4c9df014cf9dbb1f6ee087e3109e7d
andrashari-stud <andras.hari@stud.uni-corvinus.hu>
2021-03-02 02:34:03 +0100

    Status handling for socket connection added

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
   const controls = document.getElementById('controls');
   const nickNameInput = document.getElementById('nickname');
   const messageInput = document.getElementById('message');
+  const infoBox = document.getElementById('info-box');
 
   const socket = io('http://35.157.80.184:8080/');
   //localStorage.debug = '*';
@@ -52,6 +53,49 @@
       messageInput.value = '';
     }
   };
+
+  /**
+   * Status handling for socket connection
+   */
+
+  socket.on('connect', () => {
+    showInfoBox('You are connected to chatBot2000! 🧐', true);
+  });
+
+  socket.on('connecting', () => {
+    showInfoBox('Connecting is in progress to chatBot2000! 😏');
+  });
+
+  socket.on('disconnect', () => {
+    showInfoBox('You are disconnected from chatBot2000. 😳');
+  });
+
+  socket.on('reconnect', () => {
+    showInfoBox('You are successfully reconnected. 😎');
+  });
+
+  socket.on('reconnecting', () => {
+    showInfoBox('We are trying to reconnect, please wait a bit... 🙄');
+  });
+
+  socket.on('reconnect_failed', () => {
+    showInfoBox('Reconnection failed, something very bad seems to be here. 🥴');
+  });
+
+  socket.on('error', (e) => {
+    showInfoBox('There must be some error.');
+  });
+
+  const showInfoBox = (content, toBeFade) => {
+    infoBox.style.right = 0;
+    infoBox.innerHTML = content;
+    if (toBeFade) {
+      setTimeout(() => {
+        infoBox.style.right = '-100%';
+      }, 4000);
+    }
+  };
+
   // General, basic error handling
   window.addEventListener('error', function (e) {
     alert(e.error);
