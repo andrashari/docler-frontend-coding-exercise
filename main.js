08d895b1f98286c1f51e6a241bab99f0f63b5a34
andrashari-stud <andras.hari@stud.uni-corvinus.hu>
2021-03-02 02:11:24 +0100

    Handling form submission added

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 (async () => {
+  const controls = document.getElementById('controls');
+  const nickNameInput = document.getElementById('nickname');
+  const messageInput = document.getElementById('message');
 
   const socket = io('http://35.157.80.184:8080/');
   //localStorage.debug = '*';
 
+  /**
+   * Handle form submission, current user inputs
+   */
+  controls.addEventListener('submit', (e) => {
+    e.preventDefault();
+    submitMessage();
+  });
+
+  const submitMessage = () => {
+    if (messageInput.value) {
+      socket.emit('message', { message: messageInput.value, user: nickNameInput.value });
+      messageInput.value = '';
+    }
+  };
   // General, basic error handling
   window.addEventListener('error', function (e) {
     alert(e.error);
